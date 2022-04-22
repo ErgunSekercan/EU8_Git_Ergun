e45add62d48a90f149bd640bf802e2c1ba69b5f3
Ergün <ergunsekercan@gmail.com>
2022-04-22 17:24:02 +0300

    AC1 is completed

--- src/LogIn_Test_Ergun.java
+++ src/LogIn_Test_Ergun.java
@@ -2,5 +2,6 @@ public class LogIn_Test_Ergun {
 
     public static void main(String[] args) {
         System.out.println("Initial changes are done and my LogIn class is created");
+        System.out.println("AC1 testing with valid credentials is passed");
     }
 }
